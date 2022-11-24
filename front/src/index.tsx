ad796abb2e7988559b73eb3ea74107c2732a78ef
Vgyand <gyandzhunczevv@gmail.com>
2022-11-24 10:58:30 +0300

    deps init/style connect

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,13 +1,12 @@
+import "./index.scss";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <>jija</>
   </React.StrictMode>
 );
