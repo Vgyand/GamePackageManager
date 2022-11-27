e5442155de5642a7244100ac027c4f8d1ae5ddf7
Vgyand <gyandzhunczevv@gmail.com>
2022-11-27 14:00:41 +0300

    filterFix

--- front/src/components/Filter/Filter.tsx
+++ front/src/components/Filter/Filter.tsx
@@ -2,23 +2,33 @@ import { useState } from 'react'
 
 import Search from 'components/ui/Search/Search'
 
+import close from 'assets/icons/close.png'
 import filter from 'assets/icons/filter.png'
 
 import styles from './Filter.module.scss'
 
 const Filter = () => {
 	const [open, setOpen] = useState(false)
+
 	return (
 		<div>
-			<div
-				className={styles.filter}
-				style={{ display: `${open ? 'block' : 'none'}` }}
-			>
+			<div className={styles.filter}>
 				<Search />
 				selectors
 			</div>
 			<div className={styles.filterIcon} onClick={() => setOpen(!open)}>
-				<img src={filter} alt="filter" />
+				{open ? (
+					<img src={close} alt="close" />
+				) : (
+					<img src={filter} alt="filter" />
+				)}
+			</div>
+			<div
+				className={styles.filterAdapt}
+				style={{ display: `${open ? 'block' : 'none'}` }}
+			>
+				<Search />
+				selectors
 			</div>
 		</div>
 	)
