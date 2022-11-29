462f5948e8abdabc748a9efe2bf0384b5d3e655b
ReRubis <slesarenkod@gmail.com>
2022-11-30 03:29:32 +0400

    Add database, basic functions to work with it, 1 url

--- back/app/models/models.py
+++ back/app/models/models.py
@@ -1,35 +1,29 @@
 from pydantic import BaseModel
-from enum import Enum
 
 
 class Package(BaseModel):
-    id: int
-    name: str
+    id: int | None = None
+    name: str | None = None
     description: str | None = None
     download_link: str | None = None
     like_count: int | None = None
     download_count: int | None = None
-    cover_link: str | None = None
 
 
 class ListOfPackages(BaseModel):
     # list of packages client receives upon changing pages
     # Or applying filters
-    list_of_packages: dict[int, Package] | None = None
+    list_of_packages: dict[str, list[dict[str, str], dict[str, str],
+                                     dict[str, str], dict[str, str], dict[str, str]]] | None = None
+    # | None = None
 
 
 class EnteringMainPage(BaseModel):
     background_photo_link: str | None = None
     logo_link: str | None = None
     link_A: str | None = None
-    link_A: str | None = None
-    link_A: str | None = None
+    link_B: str | None = None
+    link_C: str | None = None
 
     # starting_package_list: ListOfPackages | None = None
     #  list of packages client receives upon loading the main page
-
-
-class RespModels(Enum):
-    RM_Package = Package
-    RM_Package_list = ListOfPackages
-    RM_MainPage = EnteringMainPage
