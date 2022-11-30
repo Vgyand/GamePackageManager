b39963657c2ba39456825a2616b7c2cf3cd67294
Vgyand <gyandzhunczevv@gmail.com>
2022-11-30 10:58:01 +0300

    wtf

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
+	console.log('+')
 	await app.listen(3001)
 }
 
